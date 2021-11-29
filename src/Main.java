f32cfa0fdf391da8f076027b2a60e313e9034717
Padyaa <Padya@MacBook-Andrej.local>
2021-11-29 13:13:47 +0300

    bonus colculate

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2800;
+        int miles = 20;
+        int bonus = price / miles;
+        System.out.println(bonus);
+    }
+}
+
